0617a6332091fff571f823b21d04f263404ce988
Lim, Ream <reamlim@amazon.co.uk>
2018-05-08 17:48:35 +0100

    update setup_repo

--- scripts/setup_repo.sh
+++ scripts/setup_repo.sh
@@ -1,1 +1,24 @@
-mkdir -p ~/.cmd && cd ~/.cmd && git clone git@github.com:singachea/cmd.git repo
+mkdir -p ~/.cmd && cd ~/.cmd
+
+function initialize_repo() {
+    if  [ -d ~/.cmd/repo ]; then
+        cd repo && git pull origin master
+    else
+        git clone git@github.com:singachea/cmd.git repo
+    fi
+}
+
+function add_alias() {
+    if [ ! -f ~/.zshrc ]; then
+        echo "There is no file '~/.zshrc'. You should install zsh first."
+    else
+        echo "alias cmd=\"make -f ~/.cmd/repo/Makefile\"" >> ~/.zshrc
+        # sed -i -- 's/alias cmd=.*/alias cmd=hello/g'
+    fi
+
+    source ~/.zshrc
+}
+
+initialize_repo
+add_alias
+
